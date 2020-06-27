555ad3406281439898a54ab4e3687e99d1116d31
liaosboy <nick87101227@gmail.com>
2020-06-27 21:48:20 +0800

    make it better

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web gunicorn test:app
+web: python wedding_bot.py runserver 0.0.0.0:5000
--- test.py
+++ test.py
@@ -2,8 +2,10 @@ import flask
 app = flask.Flask(__name__)
 app.config["Debug"] = True
 
+
 @app.route("/", methods=['GET'])
 def home():
     return "<h1>123</h1>"
+
 
 app.run()
